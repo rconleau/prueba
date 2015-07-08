43b1f6bbc01db0f0a54eaa299546f06c9a22b657
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-08 12:37:09 +0100

    new summit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz 2015: El Gran Reto' });
-  res.render('titlebar', { title: 'Quiz 2015: El Gran Reto: Rconleau - Ricardo de la Concepcion Leal' });
+  //res.render('titlebar', { title: 'Quiz 2015: El Gran Reto: Rconleau - Ricardo de la Concepcion Leal' });
 });
 
 router.get('/quizes/question',quizController.question);
