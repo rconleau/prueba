364bf118e030f181147ec8527afd63ea9dc11960
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-08 10:52:44 +0100

    segundo commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz 2015: El Gran Reto' });
+  res.render('titlebar', { title: 'Quiz 2015: El Gran Reto: Rconleau - Ricardo de la Concepcion Leal' });
+  
 });
 
 module.exports = router;
