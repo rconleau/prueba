3330fbcc5151b7d45f1db45e7d6f89af6e23ab52
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-11 18:19:04 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz 2015: El Gran Reto' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//router.get('/quizes/question',quizController.question);
+//router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
