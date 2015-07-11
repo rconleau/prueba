0035723c21ad96dbe697603cddcee50aec8e58da
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-11 18:35:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -33,28 +33,4 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            errors: []
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        errors: []
-    });
-});
-
 module.exports = app;
