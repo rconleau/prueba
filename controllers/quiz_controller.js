a27ebd173bb7be8c0795c0a2a10e5211b0d2168f
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-12 20:03:31 +0100

    Update quiz_controller.js

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,19 @@
-// Get quizes questions preguntas
+// GET /quizes/question
+exports.question = function(req, res){
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
 
-//exports.questions = function(req, res){
-//	res.render('quizes/question',{pregunta: 'Capital de Italia'});
-//};
+// GET /quizes/answer
+//Pregunta correcta o incorrecta
+exports.answer = function(req, res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	}else{
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+};
 
-// get Answers respuestas
-
-//exports.answers = function(req,res){
-	//if (req.query.respuesta === 'Roma'){
-		//res.render('quizes/answer', {respuesta: 'Correcto'});
-	//else {
-		//res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	//}
-//};
+// GET /author
+exports.author = function(req, res){
+	res.render('author');
+};
