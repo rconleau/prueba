3f56385b569ade6fcc788eefde14ea4f3c572fa6
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-07-12 20:20:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
-
 var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -18,13 +18,13 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -57,6 +57,4 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-
 module.exports = app;
